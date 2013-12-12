56f8456704b2ead7c8b5c1764bac0a88c1e22100
Jens Christian Hillerup <jens@hillerup.net>
2013-12-12 15:14:22 +0100

    prepared for jsdoc integration

--- natmus.js
+++ natmus.js
@@ -3,6 +3,10 @@
  * Jens Christian Hillerup, BIT BLUEPRINT - jc@bitblueprint.com
  */
 
+// Shim to avoid failures on browsers without console.
+window.console = window.console || {log: function() {}, error: function() {}};
+
+
 /**
  * The Nationalmuseet object that can emit various other objects
  * related to data gathering from Nationalmuseet.
@@ -10,12 +14,10 @@
  * @param {CIPClient} cip - A CIP.js client object
  */
 function NatMus() {
-    console.log("NatMus connection running, CIP session ID: "+this.jsessionid);
-}
-
-NatMus.prototype = {
-    constants: {
+    this.constants = {
         catch_all_alias: "any",
         layout_alias: "web"
-    }
-};
+    };
+    console.log("NatMus object initialized");
+    
+}
