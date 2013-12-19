7c3472a64aac301241dadffadf871cb624951aa3
Jens Christian Hillerup <jens@hillerup.net>
2013-12-19 09:42:46 +0100

    restructure project, resolves #9

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ NODE="node";
 JSDOC="/home/jens/node_modules/jsdoc/jsdoc.js";
 CLOSURE_COMPILER="closure";
 
-$NODE $JSDOC -d doc cip-catalog.js cip-table.js cip-layout.js cip-asset.js cip-searchresult.js cip.js natmus.js;
+$NODE $JSDOC -d doc lib/cip-catalog.js lib/cip-table.js lib/cip-layout.js lib/cip-asset.js lib/cip-searchresult.js lib/cip.js lib/natmus.js;
 
-cat header.txt > natmus.min.js;
-$CLOSURE_COMPILER qwest.js cip-common.js cip-catalog.js cip-table.js cip-layout.js cip-asset.js cip-searchresult.js cip.js natmus.js >> natmus.min.js;
+cat header.txt > lib/natmus.min.js;
+$CLOSURE_COMPILER vendor/qwest.js lib/cip-common.js lib/cip-catalog.js lib/cip-table.js lib/cip-layout.js lib/cip-asset.js lib/cip-searchresult.js lib/cip.js lib/natmus.js >> build/natmus.min.js;
