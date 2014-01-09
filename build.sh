1afc86db7d264af7f0b538af4ac60712598db073
Kræn Hansen (BIT BLUEPRINT) <kh@bitblueprint.com>
2014-01-09 16:01:35 +0100

    Renaming build to dist, as this is distribution artefacts that is the result of a build.

--- build.sh
+++ build.sh
@@ -3,20 +3,20 @@
 JSDOC="jsdoc";
 CLOSURE_COMPILER="closure";
 
-rm -rf build
-mkdir build
+rm -rf dist
+mkdir dist
 rm -rf doc
 mkdir doc
 
 $JSDOC -d doc src/cip-catalog.js src/cip-table.js src/cip-layout.js src/cip-asset.js src/cip-searchresult.js src/cip.js;
 
-cat header.txt > build/cip.min.js;
-cat header.txt > build/cip+natmus.min.js;
-$CLOSURE_COMPILER vendor/qwest.js src/cip-common.js src/cip-catalog.js src/cip-table.js src/cip-layout.js src/cip-asset.js src/cip-searchresult.js src/cip.js >> build/cip.min.js;
-$CLOSURE_COMPILER build/cip.min.js src/natmus.js >> build/cip+natmus.min.js
+cat header.txt > dist/cip.min.js;
+cat header.txt > dist/cip+natmus.min.js;
+$CLOSURE_COMPILER vendor/qwest.js src/cip-common.js src/cip-catalog.js src/cip-table.js src/cip-layout.js src/cip-asset.js src/cip-searchresult.js src/cip.js >> dist/cip.min.js;
+$CLOSURE_COMPILER dist/cip.min.js src/natmus.js >> dist/cip+natmus.min.js
 
-zip -r build/cip+natmus.zip doc;
+zip -r dist/cip+natmus.zip doc;
 
-cd build
+cd dist
 zip cip+natmus.zip cip+natmus.min.js
 zip cip+natmus.zip cip.min.js
