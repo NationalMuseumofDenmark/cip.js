7b49a875171b612bba91cf9cb174ed6adb74d2de
Kræn Hansen (BIT BLUEPRINT) <kh@bitblueprint.com>
2014-01-09 15:50:58 +0100

    Moved lib to src + changed the build script accordingly.

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-NODE="node";
-JSDOC="/home/jens/node_modules/jsdoc/jsdoc.js";
+JSDOC="jsdoc";
 CLOSURE_COMPILER="closure";
 
-rm build/cip+natmus.zip;
+rm -rf build
+mkdir build
 rm -rf doc
 mkdir doc
 
-$NODE $JSDOC -d doc lib/cip-catalog.js lib/cip-table.js lib/cip-layout.js lib/cip-asset.js lib/cip-searchresult.js lib/cip.js;
+$JSDOC -d doc src/cip-catalog.js src/cip-table.js src/cip-layout.js src/cip-asset.js src/cip-searchresult.js src/cip.js;
 
-cat header.txt > lib/natmus.min.js;
-$CLOSURE_COMPILER vendor/qwest.js lib/cip-common.js lib/cip-catalog.js lib/cip-table.js lib/cip-layout.js lib/cip-asset.js lib/cip-searchresult.js lib/cip.js >> build/cip.min.js;
-$CLOSURE_COMPILER build/cip.min.js lib/natmus.js >> build/cip+natmus.min.js
+cat header.txt > build/cip.min.js;
+cat header.txt > build/cip+natmus.min.js;
+$CLOSURE_COMPILER vendor/qwest.js src/cip-common.js src/cip-catalog.js src/cip-table.js src/cip-layout.js src/cip-asset.js src/cip-searchresult.js src/cip.js >> build/cip.min.js;
+$CLOSURE_COMPILER build/cip.min.js src/natmus.js >> build/cip+natmus.min.js
 
 zip -r build/cip+natmus.zip doc;
 
-mkdir build
 cd build
 zip cip+natmus.zip cip+natmus.min.js
 zip cip+natmus.zip cip.min.js
