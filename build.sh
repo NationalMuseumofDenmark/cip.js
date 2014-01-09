9d1b9484f774547e922a31d6f4464e505e9c2ff7
Kræn Hansen (BIT BLUEPRINT) <kh@bitblueprint.com>
2014-01-09 20:24:21 +0100

    Adding the zepto.js to the build, and removed natmus from the CIP build.

--- build.sh
+++ build.sh
@@ -11,12 +11,12 @@ mkdir doc
 $JSDOC -d doc src/cip-catalog.js src/cip-table.js src/cip-layout.js src/cip-asset.js src/cip-searchresult.js src/cip.js;
 
 cat header.txt > dist/cip.min.js;
-cat header.txt > dist/cip+natmus.min.js;
-$CLOSURE_COMPILER vendor/qwest.js src/cip-common.js src/cip-catalog.js src/cip-table.js src/cip-layout.js src/cip-asset.js src/cip-searchresult.js src/cip.js >> dist/cip.min.js;
-$CLOSURE_COMPILER dist/cip.min.js src/natmus.js >> dist/cip+natmus.min.js
+#cat header.txt > dist/cip+natmus.min.js;
+$CLOSURE_COMPILER vendor/qwest.js src/cip-common.js src/cip-catalog.js src/cip-table.js src/cip-asset.js src/cip-searchresult.js src/cip-layout.js src/cip.js vendor/zepto.min.js >> dist/cip.min.js;
+#$CLOSURE_COMPILER dist/cip.min.js src/natmus.js >> dist/cip+natmus.min.js
 
-zip -r dist/cip+natmus.zip doc;
+zip -r dist/cip.js.zip doc;
 
 cd dist
-zip cip+natmus.zip cip+natmus.min.js
+#zip cip+natmus.zip cip+natmus.min.js
 zip cip+natmus.zip cip.min.js
